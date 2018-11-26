88a0b55799472b23ff5be2821809b8424fbb8244
Kisbo_ <ezikeel@gmail.com>
2018-11-26 08:44:23 +0100

    länder

---
+++ Nukesweeper/Nukesweeper/Gameplay/Country.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace TeamStor.Nukesweeper.Gameplay
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace TeamStor.Engine
+    {
+        public class Country
+        {
+            public string Name;
+            public string ID;
+            public string[] Nukes;
+            public float NukeCountMultiplier;
+
+            public Country(string name, string id, string[] nukes, float nukeCountMultiplier)
+            {
+                Name = name;
+                ID = id;
+                Nukes = nukes;
+                NukeCountMultiplier = nukeCountMultiplier;
+            }
+        }
+    } //kaspers kod
+}
